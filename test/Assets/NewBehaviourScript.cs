3958e7b035f384ef8eeb76bf8fb8fc270eaaaf53
iGefest <igefest.com@gmail.com>
2018-05-01 13:06:06 +0300

    commit
    commit

---
+++ test/Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Transform moveThis;
+	//the layers the ray can hit
+	public LayerMask hitLayers;
+	void Update()
+	{
+		Vector3 mouse = Input.mousePosition;
+		Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+		RaycastHit hit;
+		if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayers))
+		{
+			moveThis.transform.position = hit.point;
+		}
+	}
+	
+
+}
